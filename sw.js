b99f0dbafe16b96fafc37216c8f6ac6a0cdf6eb8
ksduty <ksduty@Kennys-MacBook-Pro.local>
2026-09-14 10:04:38 -0700

    更新 X 交易員共識

---
+++ sw.js
@@ -0,0 +1,15 @@
+const VERSION = '2026-09-14T17:04:02Z';
+const CACHE = 'x-consensus-' + VERSION;
+const SHELL = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  if (req.mode === 'navigate') {
+    e.respondWith(fetch(req).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put('./', copy)); return res; })
+      .catch(() => caches.match('./')));
+    return;
+  }
+  e.respondWith(caches.match(req).then(hit => hit || fetch(req)));
+});
