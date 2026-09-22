4559b4f6fb805f1307a2a6578170a4bc28c8c247
ksduty <ksduty@Kennys-MacBook-Pro.local>
2026-09-22 07:21:30 -0700

    更新 X 交易員共識

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2026-09-21T14:25:11Z';
+const VERSION = '2026-09-22T14:13:17Z';
 const CACHE = 'x-consensus-' + VERSION;
 const SHELL = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
